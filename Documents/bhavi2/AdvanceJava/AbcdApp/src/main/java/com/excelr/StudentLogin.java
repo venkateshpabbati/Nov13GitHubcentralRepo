cef53d93e80aef74a8abcba151187ceef406ff16
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2025-02-20 12:00:40 +0530

    AbcdApp added

---
+++ Documents/bhavi2/AdvanceJava/AbcdApp/src/main/java/com/excelr/StudentLogin.java
@@ -0,0 +1,45 @@
+package com.excelr;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class StudentLogin
+ */
+@WebServlet("/StudentLogin")
+public class StudentLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		String un=request.getParameter("uname");
+		String p=request.getParameter("pwd");
+		
+		System.out.println(un);
+		System.out.println(p);
+		
+		out.println("username "+un);
+		out.println("password "+p);
+		out.print("username "+un);
+		out.print("password "+p);
+	}
+
+}
