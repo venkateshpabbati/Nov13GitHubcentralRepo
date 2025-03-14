c19f9a4b997473f7f596b960bc38551eaee45809
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2025-03-14 11:39:03 +0530

    DEc18Appadded

---
+++ Documents/bhavi2/AdvanceJava/Dec18FirstJavaApp/src/com/excelr/Car.java
@@ -0,0 +1,16 @@
+package com.excelr;
+
+public class Car {
+
+	public Car() {
+		System.out.println("car constructor executed...");
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ Documents/bhavi2/AdvanceJava/Dec18FirstJavaApp/src/com/excelr/Main.java
@@ -0,0 +1,13 @@
+package com.excelr;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome");
+		
+		Car c1=new Car();
+
+	}
+
+}
